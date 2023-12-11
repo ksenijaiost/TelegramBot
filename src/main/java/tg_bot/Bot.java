cbca2373a94da62b1aa0be5b3ff5b8b9e2fa0185
Iost Kseniya <iostka@bcs-bank.com>
2023-12-11 09:58:33 +0700

    refactoring

--- src/main/java/tg_bot/Bot.java
+++ src/main/java/tg_bot/Bot.java
@@ -6,7 +6,7 @@ import tg_bot.rest.dao.ListOfSending;
 import java.util.List;
 
 public interface Bot {
-    public List<String> sendMsg(List<ListOfSending> listOfSending);
+    List<String> sendMsg(List<ListOfSending> listOfSending);
 
-    public List<String> oneMsg(List<ListOfChatId> chatIds, String text);
+    List<String> oneMsg(List<ListOfChatId> chatIds, String text);
 }
