d7b6067cf58239dc5f924f4bf6998ada468a65b2
Ксения <ksenijaiost@gmail.com>
2025-03-10 20:50:21 +0700

    Всё сломала, начну с 0

--- src/main/java/tg_bot/Application.java
+++ src/main/java/tg_bot/Application.java
@@ -1,13 +1,21 @@
 package tg_bot;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+import tg_bot.bot.Bot;
 
-@SpringBootApplication
-@EnableFeignClients
+import java.util.Map;
+
 public class Application {
+    private static final Map<String, String> getenv = System.getenv();
+
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        try {
+            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+            botsApi.registerBot(new Bot(getenv.get("BOT_NAME"), getenv.get("BOT_TOKEN")));
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ src/main/java/tg_bot/bot/Bot.java
@@ -0,0 +1,72 @@
+package tg_bot.bot;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.telegram.telegrambots.extensions.bots.commandbot.TelegramLongPollingCommandBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+/**
+ * Собственно, бот
+ */
+public final class Bot extends TelegramLongPollingCommandBot {
+    private Logger logger = LoggerFactory.getLogger(Bot.class);
+
+    private final String BOT_NAME;
+    private final String BOT_TOKEN;
+
+    public Bot(String botName, String botToken) {
+        super();
+        logger.debug("Конструктор суперкласса отработал");
+        this.BOT_NAME = botName;
+        this.BOT_TOKEN = botToken;
+        logger.debug("Имя и токен присвоены");
+
+        logger.info("Бот создан!");
+    }
+
+    @Override
+    public String getBotToken() {
+        return BOT_TOKEN;
+    }
+
+    @Override
+    public String getBotUsername() {
+        return BOT_NAME;
+    }
+
+    /**
+     * Ответ на запрос, не являющийся командой
+     */
+    @Override
+    public void processNonCommandUpdate(Update update) {
+        Message msg = update.getMessage();
+        Long chatId = msg.getChatId();
+//        String userName = Utils.getUserName(msg);
+
+//        String answer = nonCommand.nonCommandExecute(chatId, userName, msg.getText());
+//        setAnswer(chatId, userName, answer);
+    }
+
+
+    /**
+     * Отправка ответа
+     * @param chatId id чата
+     * @param userName имя пользователя
+     * @param text текст ответа
+     */
+    private void setAnswer(Long chatId, String userName, String text) {
+        SendMessage answer = new SendMessage();
+        answer.setText(text);
+        answer.setChatId(chatId.toString());
+        try {
+            execute(answer);
+        } catch (TelegramApiException e) {
+            logger.error(String.format("Ошибка %s. Сообщение, не являющееся командой. Пользователь: %s", e.getMessage(),
+                    userName));
+            e.printStackTrace();
+        }
+    }
+}
